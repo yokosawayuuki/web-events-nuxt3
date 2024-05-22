4db659f83c5ea08357412697db66df33edd12352
yokosawayuuki <yokosawa0505@gmail.com>
2024-05-23 03:24:30 +0900

    jsonのdescriptionの中身をulタグ,aタグに分割

--- server/api/events.js
+++ server/api/events.js
@@ -2,22 +2,51 @@ import { google } from 'googleapis';
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
-  const calendarId = config.public.googleCalendarId; // 環境変数からカレンダーIDを取得
-  const apiKey = config.public.googleApiKey; // 環境変数からAPIキーを取得
-
+  const calendarId = config.public.googleCalendarId;
+  const apiKey = config.public.googleApiKey;
   const calendar = google.calendar({ version: 'v3', auth: apiKey });
 
   try {
     const res = await calendar.events.list({
       calendarId,
       timeMin: (new Date()).toISOString(),
-      maxResults: 10,
+      maxResults: 30,
       singleEvents: true,
       orderBy: 'startTime',
     });
-    return res.data.items;
+
+    const processedEvents = processEvents(res.data.items);
+    return processedEvents;
   } catch (error) {
     console.error('Error fetching calendar events:', error);
     throw error;
   }
 });
+
+function processEvents(events) {
+  return events.map(event => {
+    const description = event.description;
+    const descriptionParts = [];
+
+    // a要素を抽出
+    const aPattern = /<a\s*href=["'](.*?)["']>(.*?)<\/a>/gs;
+    const aMatches = [...description.matchAll(aPattern)];
+    for (const match of aMatches) {
+      const [fullMatch, link, content] = match;
+      descriptionParts.push({ link });
+    }
+
+    // ul要素を抽出
+    const ulPattern = /<ul>(.*?)<\/ul>/gs;
+    const ulMatches = [...description.matchAll(ulPattern)];
+    for (const match of ulMatches) {
+      const [fullMatch, content] = match;
+      descriptionParts.push({ cat: content });
+    }
+
+    return {
+      ...event,
+      description: descriptionParts
+    };
+  });
+}
